4d6de56dbacb9a6bf9152436dac6ac30d799f559
Dev2 <dev2@gmail.com>
2023-07-10 20:53:55 +0530

    Developer2 add Hello statement

--- DemoProject1/src/com/nt/test/Test.java
+++ DemoProject1/src/com/nt/test/Test.java
@@ -4,6 +4,7 @@ import com.nt.beans.Utility;
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		Utility u=new Utility();
 		System.out.println(u.getSysDate());
 	}
